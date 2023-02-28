9048aab93e7d8094a607f1cff3a69d3c1bff8b1d
LeoJung <leojung0909@gmail.com>
2023-02-28 13:36:21 +0900

    [util] download youtube videos
    - using pytube

---
+++ utils/youtube/download_videos.py
@@ -0,0 +1,18 @@
+from pytube import Playlist
+import pdb
+
+DOWNLOAD_FOLDER = r"D:\00. 개인\01. 강의\01. 운영체제(널널한 개발자)"
+
+pl = Playlist('https://www.youtube.com/watch?v=ZrNp9Be83qQ&list=PLXvgR_grOs1DGFOeD792kHlRml0PhCe9l')
+total = len(pl)
+for i, video in enumerate(pl.videos, 1):
+    if i<26: continue
+    print("[%s/%s]" % (i, total))
+    try:
+        high_res_video = video.streams.get_highest_resolution()
+    except Exception as e:
+        print("Error msg: {e}".format(e=e))
+        continue
+
+    print("mimetype: " + high_res_video.mime_type)
+    high_res_video.download(DOWNLOAD_FOLDER)
